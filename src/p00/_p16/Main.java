83c80a3b694e46c337fdfbb719e5f21742e7da98
wisdom08 <ji4168aw@gmail.com>
2020-11-02 07:41:18 +0900

    별 출력1

---
+++ src/p00/_p16/Main.java
@@ -0,0 +1,15 @@
+package p00._p16;
+
+//별 출력1
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
