786dada304cd5756fb74b9b9accb1552a80674ca
wisdom08 <wisdom08@kakao.com>
2020-11-08 08:35:29 +0900

    팰린드롬 Palindrome

--- src/p00/_22/Main.java
+++ src/p00/_22/Main.java
@@ -1,7 +1,21 @@
 package p00._22;
 
+//팰린드롬 Palindrome
 public class Main {
     public static void main(String[] args) {
+
+        String n = "12321";
+        int len = n.length();
+        boolean flag = true;
+
+        for (int i = 0; i < len / 2; i++) {
+            if (n.charAt(i) != n.charAt(len - i - 1)) {
+                flag = false;
+            }
+
+        }
+
+        System.out.println(flag);
 
     }
 }
