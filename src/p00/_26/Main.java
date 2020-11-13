12eea5eeaf57d37fa1f5c11148de24e01c9c7909
wisdom08 <wisdom08@kakao.com>
2020-11-14 08:37:46 +0900

    상수

--- src/p00/_26/Main.java
+++ src/p00/_26/Main.java
@@ -4,28 +4,24 @@ package p00._26;
 public class Main {
     public static void main(String[] args) {
         int a = 734;
-        int b = 893;
 
-        int arrA[] = new int [3];
-        int arrB[] = new int [3];
-
-        int lenA = 0;
+        int arr[] = new int[3];
+        int len = 0;
         do{
-            arrA[lenA] = 0;
-            a /= 10;
-            lenA++;
+            arr[len] = a % 10;
+            a/=10;
+            len++;
         } while(a>0);
 
-        int tranA = 0;
+        int result = 0;
         int mux = 1;
-
-        for (int i = arrA.length - 1; i >= 0; i--) {
-            tranA += arrA[i]*mux;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            result += arr[i]*mux;
             mux*=10;
         }
 
-        System.out.println("tranA="+tranA);
-
+        System.out.println("result = " + result);
 
     }
 }
+
