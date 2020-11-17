148b32f234969f47fafaf099a94094b106e54a67
wisdom08 <wisdom08@kakao.ocm>
2020-11-17 23:09:09 +0900

    더하기 & 마이너스

---
+++ src/p00/_30/Main.java
@@ -0,0 +1,25 @@
+package p00._30;
+
+// 더하기 & 마이너스
+public class Main {
+    public static void main(String[] args) {
+        int num = 9;
+        boolean flag = true;
+        for (int i = 1; i <= num; i++) {
+            // 1+2-3+4-5 ...
+
+            if (i == num) {
+                System.out.print(i);
+                break;
+            }
+
+            if (flag) {
+                System.out.print(i+"+");
+                flag = !flag;
+            } else {
+                System.out.print(i+"-");
+                flag = !flag;
+            }
+        }
+    }
+}
